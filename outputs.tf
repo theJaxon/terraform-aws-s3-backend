19f43031c83181e115d930a554e0dca4c98df4dd
Jaxon <mohamedmostels@gmail.com>
2025-01-02 18:28:55 +0100

    Add role_arn

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,8 @@
 output "configurations" {
   value = {
-    bucket = aws_s3_bucket.s3_bucket.bucket
-    region = data.aws_region.current_region.name
+    bucket         = aws_s3_bucket.s3_bucket.bucket
+    region         = data.aws_region.current_region.name
+    role_arn       = aws_iam_role.iam_role.arn
     dynamodb_table = aws_dynamodb_table.dynamodb_table.name
   }
 }
