f1c0d4531d443bbe3b73d60aab1c5fb103f303ef
Jaxon <mohamedmostels@gmail.com>
2025-01-03 19:12:04 +0100

    Add description to output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "configurations" {
+  description = "Should be used as an input when using the S3 backend provider. Contains S3 bucket name, used IAM role ARN, dynamodb table name and AWS region where the resources were deployed."
   value = {
     bucket         = aws_s3_bucket.s3_bucket.bucket
     region         = data.aws_region.current_region.name
